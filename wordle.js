1f78b205b0a025f71ff540c5b11d7acf3dfefdab
Ryan Pham <rpham817@gmail.com>
2025-02-12 03:31:04 -0800

    responsive changes

--- wordle.js
+++ wordle.js
@@ -53,7 +53,7 @@ function initialize() {
     let keyboard = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]
+        ["Enter ", "Z", "X", "C", "V", "B", "N", "M", "⌫"]
     ];
 
     for (let i = 0; i < keyboard.length; i++) {
